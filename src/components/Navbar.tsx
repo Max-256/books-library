7d6415467e0c72305a7fa125c3750af0739999fa
Opio Maxwell <opiomaxwell11122@gmail.com>
2023-04-05 20:44:26 +0300

    build the search input comoponent

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image } from "@chakra-ui/react";
 import image from "../assets/library-logo.png";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
 const Navbar = () => {
   return (
     <HStack justifyContent={"space-between"}>
       <Image boxSize="60px" src={image} />
+      <SearchInput />
       <ColorModeSwitch />
     </HStack>
   );
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,7 +1,33 @@
-import { Input } from "@chakra-ui/react";
+import {
+  Input,
+  InputGroup,
+  InputLeftAddon,
+  InputLeftElement,
+} from "@chakra-ui/react";
+import { useRef } from "react";
+import { BsSearch } from "react-icons/bs";
 
 const SearchInput = () => {
-  return <Input variant="filled" placeholder="Search books..." />;
+  const ref = useRef(null);
+
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        if (ref.current) console.log(ref.current);
+      }}
+    >
+      <InputGroup>
+        <InputLeftElement children={<BsSearch />} />
+        <Input
+          ref={ref}
+          borderRadius={20}
+          variant="filled"
+          placeholder="Search books..."
+        />
+      </InputGroup>
+    </form>
+  );
 };
 
 export default SearchInput;
